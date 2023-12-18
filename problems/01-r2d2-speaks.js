e49641ec8a89ff9b93aa006a4a265c7ab14fc486
Nikita Kastyshyn <4luckynikita@gmail.com>
2023-12-17 16:22:14 -0800

    problem 01 done

--- problems/01-r2d2-speaks.js
+++ problems/01-r2d2-speaks.js
@@ -16,10 +16,23 @@
 
 function r2d2Speaks(code) {
     // Your code here 
+    if(code.length==0) return;
+        let popped = code.pop();
+        //console.log(popped);
+        if(popped == "0"){
+            console.log('beep');
+            return setTimeout(() => {r2d2Speaks(code)}, 400);
+        }
+        else {
+            console.log('boop');
+            return setTimeout(() => {r2d2Speaks(code)}, 800);
+        }
+    
+    //return r2d2Speaks(code);
 }
 
-let code = [0, 1, 1, 0];
-r2d2Speaks(code);
+// let code = [0, 1, 1, 0];
+// r2d2Speaks(code);
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {

d03964c7734e29b57fe6438c22f9b2d96eafd5dd
Nikita Kastyshyn <4luckynikita@gmail.com>
2023-12-17 17:10:08 -0800

    01 done dont look at 02

--- problems/02-recursive-sort.js
+++ problems/02-recursive-sort.js
@@ -3,16 +3,27 @@
 
     Given an array of numbers, write a function that returns a new array of numbers sorted from lowest to highest.
 
-    console.log(sort([])) // prints []
+    
+*/
+
+function sort(nums, final = []) {
+    // Your code here 
+    if (nums = []) return final;
+
+    if(final = []) final = nums.shift();
+    if(final ){
+        for(let i = 0; i < length;i++){
+            
+        }
+    }
+    //nums.unshift(value);
+    return nums;
+    
+}
+console.log(sort([])) // prints []
     console.log(sort([9])) // prints [9]
     console.log(sort([5, 4, 3, 2, 1])) // prints [1, 2, 3, 4, 5]
     console.log(sort([14, 5, 10, 6, 3, 4, 21, 16, 9])); // prints [ 3, 4, 5, 6, 9, 10, 14, 16, 21 ]
-*/
-
-function sort(nums) {
-    // Your code here 
-}
-
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
     module.exports = sort;

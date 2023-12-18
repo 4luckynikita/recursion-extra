f5071c2ea267c15f38ae15a298efbda0b029ebc2
Nikita Kastyshyn <4luckynikita@gmail.com>
2023-12-17 17:47:50 -0800

    02 miraculously somehow completed

--- problems/02-recursive-sort.js
+++ problems/02-recursive-sort.js
@@ -8,22 +8,28 @@
 
 function sort(nums, final = []) {
     // Your code here 
-    if (nums = []) return final;
+    if (nums.length == 0) return final;
 
-    if(final = []) final = nums.shift();
-    if(final ){
-        for(let i = 0; i < length;i++){
-            
+    let largest = -Infinity;
+    let indexOfLargest = 0;
+
+    for(let i = 0; i < nums.length;i++){
+        if (largest < nums[i]){
+            largest = nums[i];
+            indexOfLargest = i;
         }
     }
-    //nums.unshift(value);
-    return nums;
+
+    nums.splice(indexOfLargest,1);
+    final.unshift(largest);
+    return sort(nums,final);
     
 }
-console.log(sort([])) // prints []
-    console.log(sort([9])) // prints [9]
-    console.log(sort([5, 4, 3, 2, 1])) // prints [1, 2, 3, 4, 5]
-    console.log(sort([14, 5, 10, 6, 3, 4, 21, 16, 9])); // prints [ 3, 4, 5, 6, 9, 10, 14, 16, 21 ]
+
+// console.log(sort([])) // prints []
+// console.log(sort([9])) // prints [9]
+// console.log(sort([5, 4, 3, 2, 1])) // prints [1, 2, 3, 4, 5]
+// console.log(sort([14, 5, 10, 6, 3, 4, 21, 16, 9])); // prints [ 3, 4, 5, 6, 9, 10, 14, 16, 21 ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
     module.exports = sort;
